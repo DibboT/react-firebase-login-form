9f70478e42265b3ebaa4bad02274c7b21a289cea
Dibbo T <dibbotripura7@gmail.com>
2023-10-01 10:44:52 +0600

    login provider updated

--- src/providers/LoginProvider.jsx
+++ src/providers/LoginProvider.jsx
@@ -1,14 +1,15 @@
 import { createContext } from "react";
+import PropTypes from 'prop-types';
 
 
-const LoginContext =createContext(null);
+export const LoginContext =createContext(null);
 
-const LoginProvider = () => {
+const LoginProvider = ({ children }) => {
     const loginInfo = {name: 'Hello'}
     return (
 
             <LoginContext.Provider value={loginInfo}>
-
+                {children}
             </LoginContext.Provider>
             
     
@@ -17,8 +18,13 @@ const LoginProvider = () => {
 
 export default LoginProvider;
 
+LoginProvider.propTypes = {
+    children: PropTypes.node,
+}
+
 /**
- * 1. reate context
+ * 1. reate context and export it
  * 2. set provider with value
- * 3. 
+ * 3. use LoginProvider in main.jsx file
+ * 4. access children in LoginProvider component as children and use it in the middle of the provider
  */
